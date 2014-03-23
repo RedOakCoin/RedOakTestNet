b9647d4468f9d0e0984f717e38c0bc3ca1a911da
RedOakCoin <RedOakCoin@Live.Com>
2014-03-23 19:22:54 -0400

    Running TestNet On RedOakNetwork
    Contains _WinBuilder_ & *.Exe for ROC
    NOTE: Run Only With -testnet
    NOTE: _WinBuilder_ Has been altered for my *Deps
    NOTE: Hard Coded "USE_UPNP=-" Into .Bat

---
+++ src/checkpoints.cpp
@@ -0,0 +1,60 @@
+// Copyright (c) 2009-2012 The Bitcoin developers
+// Copyright (c) 2011-2012 redoakcoin Developers
+// Distributed under the MIT/X11 software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#include <boost/assign/list_of.hpp> // for 'map_list_of()'
+#include <boost/foreach.hpp>
+
+#include "checkpoints.h"
+
+#include "main.h"
+#include "uint256.h"
+
+namespace Checkpoints
+{
+    typedef std::map<int, uint256> MapCheckpoints;
+
+    //
+    // What makes a good checkpoint block?
+    // + Is surrounded by blocks with reasonable timestamps
+    //   (no blocks before with a timestamp after, none after with
+    //    timestamp before)
+    // + Contains no strange transactions
+    //
+    static MapCheckpoints mapCheckpoints =
+        boost::assign::map_list_of // TODO: needs to adjusted for checkpoint checks, also see main.cpp
+        (     0, uint256("0x1ed988fdaad31f8ae53f1858b8470514b252b251d80778d0c1fce39c622dabeb"))
+	//(     1, uint256("0x3876830dd3de6ffdcb7d9ef8929d72d0f9c62d959380f4464312f641bd096a39"))
+        ;
+
+    bool CheckBlock(int nHeight, const uint256& hash)
+    {
+        if (fTestNet) return true; // Testnet has no checkpoints
+
+        MapCheckpoints::const_iterator i = mapCheckpoints.find(nHeight);
+        if (i == mapCheckpoints.end()) return true;
+        return hash == i->second;
+    }
+
+    int GetTotalBlocksEstimate()
+    {
+        if (fTestNet) return 0;
+		return 0;
+        return mapCheckpoints.rbegin()->first;
+    }
+
+    CBlockIndex* GetLastCheckpoint(const std::map<uint256, CBlockIndex*>& mapBlockIndex)
+    {
+        if (fTestNet) return NULL;
+
+        BOOST_REVERSE_FOREACH(const MapCheckpoints::value_type& i, mapCheckpoints)
+        {
+            const uint256& hash = i.second;
+            std::map<uint256, CBlockIndex*>::const_iterator t = mapBlockIndex.find(hash);
+            if (t != mapBlockIndex.end())
+                return t->second;
+        }
+        return NULL;
+    }
+}
